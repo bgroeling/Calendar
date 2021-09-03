f6a6a12f577a41eaf7aee84e5c330576da13f3d2
bgroeling <84939419+bgroeling@users.noreply.github.com>
2021-09-03 13:14:55 -0600

    finishing details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-console.log(localStorage)
+var saveBtn = document.getElementsByClassName("saveBtn");
+var description = $(".description");
+var currentHour = moment().hours();
+
+$(saveBtn).click(function(){
+    var input = $(this).siblings(".description").val()
+    var id = $(this).parent().attr("id")
+    localStorage.setItem(id, input)
+})
+
+$(".time-block").each(function(){
+    var id = $(this).attr("id")
+    var item = localStorage.getItem(id)
+    $(this).find (description).val(item)
+    if(id>currentHour) {
+        $(this).addClass("future")
+    }else if(id === currentHour){
+        $(this).addClass("present")
+    }else {
+        $(this).addClass("past")
+    }
+})
